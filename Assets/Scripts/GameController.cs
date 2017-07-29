eab69ad8d2ada4491ef81db943dfedaa8d542126
Sam Hindmarsh <sam@hndmr.sh>
2017-07-29 16:06:30 +1200

    Initial game set up

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public Player player1;
+    public Player player2;
+
+    public GameObject level;
+
+    public Text instructionsText;
+
+    void Start()
+    {
+        ChooseStartingLocation(player1);
+    }
+
+    private void ChooseStartingLocation(Player player)
+    {
+        instructionsText.color = player.highlightColour;
+        instructionsText.text = player.playerName;
+
+
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public string playerName;
+
+    public Color controlledColour;
+    public Color highlightColour;
+    public Color startingAreaColour;
+
+}
